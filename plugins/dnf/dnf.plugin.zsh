56239f9d7b57c3d15f736616dfe3a1407fb22502
Oliver Ruste Jahren <oliverrj@uio.no>
2026-05-25 16:33:34 +0200

    fix(dnf): 'dnfli' uses different syntax for DNF 5.

--- plugins/dnf/dnf.plugin.zsh
+++ plugins/dnf/dnf.plugin.zsh
@@ -5,7 +5,6 @@ local dnfprog="dnf"
 command -v dnf5 > /dev/null && dnfprog=dnf5
 
 alias dnfl="${dnfprog} list"                       # List packages
-alias dnfli="${dnfprog} list installed"            # List installed packages
 alias dnfmc="${dnfprog} makecache"                 # Generate metadata cache
 alias dnfp="${dnfprog} info"                       # Show package information
 alias dnfs="${dnfprog} search"                     # Search package
@@ -18,10 +17,12 @@ alias dnfc="sudo ${dnfprog} clean all"             # Clean cache
 # Conditional aliases based on dnfprog value
 if [[ "${dnfprog}" == "dnf5" ]]; then
     alias dnfgl="${dnfprog} group list"            # List package groups (dnf5)
+    alias dnfli="${dnfprog} list --installed"      # List installed packages (dnf5)
     alias dnfgi="sudo ${dnfprog} group install"    # Install package group (dnf5)
     alias dnfgr="sudo ${dnfprog} group remove"     # Remove package group (dnf5)
 else
     alias dnfgl="${dnfprog} grouplist"             # List package groups (dnf)
+    alias dnfli="${dnfprog} list installed"        # List installed packages (dnf)
     alias dnfgi="sudo ${dnfprog} groupinstall"     # Install package group (dnf)
     alias dnfgr="sudo ${dnfprog} groupremove"      # Remove package group (dnf)
 fi
